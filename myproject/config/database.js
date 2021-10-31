2addb5ea7a15e3f6793c7278b929bf157e496d3b
Vili Raunola <raunolavili@gmail.com>
2021-10-31 17:31:47 +0200

    unohin vaihtaa mongon osoitteen

--- myproject/config/database.js
+++ myproject/config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/myproject',
+    database: 'mongodb+srv://admin:admin@testicluster.sr1mh.mongodb.net/myproject?retryWrites=true&w=majority',
     // mongodb+srv://admin:admin@testicluster.sr1mh.mongodb.net/myproject?retryWrites=true&w=majority
     // mongodb://localhost:27017/myproject
     secret: 'minunsalaisuus'
