b0f8ef4d99cc12b751128b85aa530b70e97c853f
Vili Raunola <raunolavili@gmail.com>
2021-10-31 17:29:58 +0200

    kommentoin pois

--- myproject/app.js
+++ myproject/app.js
@@ -62,9 +62,9 @@ app.get('/', (req, res) => {
 });
 
 // This is for when the build is ready
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public/index.html'));
-// });
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 //Creating the server
 app.listen(port, () =>{
